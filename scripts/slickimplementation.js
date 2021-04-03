4401a7dd86ef6f45e9611a5543255f90f41c3c0d
smullangi213 <52145216+smullangi213@users.noreply.github.com>
2021-04-03 17:18:37 -0500

    Header Responsiveness Addition
    Using jQuery and CSS, created a smaller hamburger menu for small phone displays.

---
+++ scripts/slickimplementation.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        centerMode: true,
+        slidesToShow: 2,
+        arrows: false,
+        dots: true
+    });
+  });
