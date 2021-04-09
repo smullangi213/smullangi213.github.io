359ee4b25b06e5155283ec2c6e0ab1cca2d1f656
smullangi213 <52145216+smullangi213@users.noreply.github.com>
2021-04-09 09:51:30 -0500

    Content addition

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,3 +1,7 @@
 $(".hamburgermenu").click(function(){
     $("nav").toggleClass("responsive");
 })
+
+$(".purchasebutton").click(function(){
+    alert("Because this is a mock website for Technology Student Association's Webmaster Competition 2021, a real purchase page has not been included. Still, thank you for your interest in our company's mission.");
+})
