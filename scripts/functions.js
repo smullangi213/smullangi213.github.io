ac9e908dec8e4a1e510279bc25973c3589f889de
smullangi213 <52145216+smullangi213@users.noreply.github.com>
2021-04-10 21:43:17 -0500

    Document and submission update

--- scripts/functions.js
+++ scripts/functions.js
@@ -5,3 +5,7 @@ $(".hamburgermenu").click(function(){
 $(".purchasebutton").click(function(){
     alert("Because this is a mock website for Technology Student Association's Webmaster Competition 2021, a real purchase page has not been included. Still, thank you for your interest in our company's mission.");
 })
+
+$("input[type=submit]").click(function(){
+    alert("Because this is a mock website for Technology Student Association's Webmaster Competition 2021, contact information has not actually been submitted. Still, thank you for your interest in our company's mission.");
+})
